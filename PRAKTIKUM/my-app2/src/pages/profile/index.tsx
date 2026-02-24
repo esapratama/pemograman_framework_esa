80981306dfd2efe150c0f6e90f72242f0d9e353b
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-02-24 20:15:43 +0700

    Add Tugas 1 – Routing

---
+++ PRAKTIKUM/my-app2/src/pages/profile/index.tsx
@@ -0,0 +1,53 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from "next/link";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Profile() {
+  return (
+    <div className={styles.container}>
+      <h3>Profil Mahasiswa</h3>
+
+      <div className={styles.formGroup}>
+        <p><strong>Nama:</strong> Esa Pratama Putri</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>NIM:</strong> 2341720061</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Kelas:</strong> TI-3D</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Program Studi:</strong> D4 Teknik Informatika</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Jurusan:</strong> Teknologi Informasi</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Kampus:</strong> Politeknik Negeri Malang</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Semester:</strong> 6</p>
+      </div>
+
+      <div className={styles.formGroup}>
+        <p><strong>Angkatan:</strong> 2023</p>
+      </div>
+
+      <br />
+
+      <Link href="/profile/edit">
+        <button className={styles.button}>
+          Edit Profile
+        </button>
+      </Link>
+    </div>
+  );
+}

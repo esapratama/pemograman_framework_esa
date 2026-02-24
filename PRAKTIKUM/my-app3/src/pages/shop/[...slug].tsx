0fd48eef7720c251b29208c66c4dade997afa93b
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-02-24 22:33:39 +0700

    Add Langkah 3 – Pengujian Catch-All Route

--- PRAKTIKUM/my-app3/src/pages/shop/[...slug].tsx
+++ PRAKTIKUM/my-app3/src/pages/shop/[...slug].tsx
@@ -7,7 +7,7 @@ const halamanToko = () => {
   return (
     <div>
       <h1>Halaman Toko</h1>
-      <p>Toko: {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}</p> {/* menggunakan backtick bukan petik satu*/}
+      <p>Toko: {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}</p> {/* menggunakan backtick bukan petik satu */}
     </div>
   );
 };

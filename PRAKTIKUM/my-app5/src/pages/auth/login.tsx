c4cc087311c8dfe8264ea9f400944b8384069401
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-03-02 01:47:59 +0700

    Add Tailwind CSS

--- PRAKTIKUM/my-app5/src/pages/auth/login.tsx
+++ PRAKTIKUM/my-app5/src/pages/auth/login.tsx
@@ -1,12 +1,24 @@
-import TampilanLogin from "../views/auth/login";
+import Link from "next/link";
+import { useRouter } from "next/router";
+// import styles from './login.module.css';
+import styles from './login.module.scss';
 
-const halamanLogin = () => {
-  
-  return (
-    <>
-    <TampilanLogin />
-    </>
-  );
-};
+const TampilanLogin = () => {
+    const {push} = useRouter();
 
-export default halamanLogin;
+    const handleLogin = () => {
+        // logic login disini
+        push('/produk');
+    }
+
+    return (
+        <div className={styles.login}>
+            <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
+            <button onClick={() => handleLogin()}>Login</button> <br />
+            <h1 style={{color:"red", border:"1px solid red", borderRadius:"5px", padding: "5px"}}> belum punya akun</h1>
+            <Link href={"/auth/register"}>Ke Halaman Register</Link>
+        </div>
+    );
+}
+
+export default TampilanLogin

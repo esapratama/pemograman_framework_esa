d33f120ce02d8a95c641caafb7bc0e5c1eb4ba74
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-03-02 18:58:49 +0700

    Add Tugas 2

--- PRAKTIKUM/my-app5/src/pages/produk/index.tsx
+++ PRAKTIKUM/my-app5/src/pages/produk/index.tsx
@@ -1,28 +1,3 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import ProdukView from "../views/produk/index";
 
-const Produk = () => {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const isLogin = localStorage.getItem("isLogin");
-
-    if (!isLogin) {
-      router.push("/auth/login");
-    } else {
-      setLoading(false);
-    }
-  }, [router]);
-
-  if (loading) return <p>Loading...</p>;
-
-  return (
-    <div>
-      <h1>Produk User Page</h1>
-      <p>Selamat datang, kamu sudah login 🎉</p>
-    </div>
-  );
-};
-
-export default Produk;
+export default ProdukView;

0d99b7cb8ed1c6c97aed3b668fb5331adf666add
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-03-02 21:04:18 +0700

    Add Langkah 5 – Styling Halaman 404

--- PRAKTIKUM/my-app6/src/pages/404.tsx
+++ PRAKTIKUM/my-app6/src/pages/404.tsx
@@ -1,10 +1,13 @@
+import styles from "@/styles/404.module.scss";
+
 const Custom404 = () => {
-  return (
-    <div>
-      <h1>404 - Halaman Tidak Ditemukan</h1>
-      <p>Maaf, halaman yang Anda cari tidak ada.</p>
-    </div>
-  );
+    return (
+        <div className={styles.error}>
+            <img src="/page-not-found.png" alt="404" className={styles.error__image} />
+            <h1>404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf, halaman yang Anda cari tidak ada.</p>
+        </div>
+    );
 };
 
 export default Custom404;

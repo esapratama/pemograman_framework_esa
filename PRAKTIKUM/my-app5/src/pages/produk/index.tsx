839566c35e0e4c8d0fb1fb3171b184253c35e6e0
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-03-02 19:15:16 +0700

    Add Tugas 3

--- PRAKTIKUM/my-app5/src/pages/produk/index.tsx
+++ PRAKTIKUM/my-app5/src/pages/produk/index.tsx
@@ -1,3 +1,19 @@
 import ProdukView from "../views/produk/index";
 
-export default ProdukView;
+const ProdukPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600 mb-4">
+        Halaman Produk
+      </h1>
+
+      <div className="bg-white shadow-lg rounded-lg p-6 w-80 text-center">
+        <p className="text-gray-700">
+          Selamat datang di halaman produk 
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProdukPage;

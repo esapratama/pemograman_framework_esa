64432f2be12e39d4e503e20a3594e1c1e451f547
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-03-03 19:34:00 +0700

    Add Langkah 3 – Fetch Data API di Frontend

--- PRAKTIKUM/my-app7/src/pages/produk/index.tsx
+++ PRAKTIKUM/my-app7/src/pages/produk/index.tsx
@@ -1,19 +1,71 @@
-import ProdukView from "../views/produk/index";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-const ProdukPage = () => {
+type ProductType = {
+  id: string;
+  nama: string;
+  harga: number;
+  ukuran: string;
+  warna: string;
+};
+
+const Kategori = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     push("/auth/login");
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    fetch("/api/produk")
+      .then((response) => response.json())
+      .then((responsedata) => {
+        // console.log("Data produk:", responsedata.data);
+        setProducts(responsedata.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching produk:", error);
+      });
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-600 mb-4">
-        Halaman Produk
-      </h1>
-
-      <div className="bg-white shadow-lg rounded-lg p-6 w-80 text-center">
-        <p className="text-gray-700">
-          Selamat datang di halaman produk 
-        </p>
-      </div>
+    <div>
+      <h1>Daftar Produk</h1>
+      {products.map((product: ProductType) => (
+        <div key={product.id}>
+          <h2>{product.nama}</h2>
+          <p>Harga: {product.harga}</p>
+          <p>Ukuran: {product.ukuran}</p>
+          <p>Warna: {product.warna}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default ProdukPage;
+export default Kategori;
+
+
+// import ProdukView from "../views/produk/index";
+
+// const ProdukPage = () => {
+//   return (
+//     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+//       <h1 className="text-3xl font-bold text-blue-600 mb-4">
+//         Halaman Produk
+//       </h1>
+
+//       <div className="bg-white shadow-lg rounded-lg p-6 w-80 text-center">
+//         <p className="text-gray-700">
+//           Selamat datang di halaman produk 
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProdukPage;

bf622966684eaecbe76461df723544dc9018f01d
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-03-04 22:22:20 +0700

    Add File My-App8

---
+++ PRAKTIKUM/my-app8/src/pages/views/produk/MainSection.tsx
@@ -0,0 +1,12 @@
+import styles from "./produk.module.scss";
+
+const MainSection = () => {
+  return (
+    <section className={styles.main}>
+      <h2>Daftar Produk</h2>
+      <p>Ini adalah Main Section</p>
+    </section>
+  );
+};
+
+export default MainSection;

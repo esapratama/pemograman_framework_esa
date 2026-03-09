7b2b3af504e3aabfebf1b2d932b07972a2199b1f
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-03-09 22:09:24 +0700

    Add Bagian 1 – Membuat Dynamic Route

--- PRAKTIKUM/my-app11/src/views/produk/index.tsx
+++ PRAKTIKUM/my-app11/src/views/produk/index.tsx
@@ -1,4 +1,5 @@
 import styles from "../../pages/produk/produk.module.scss";
+import Link from "next/link";
 
 type ProductType = {
   id: string;
@@ -16,7 +17,11 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
         {products.length > 0 ? (
           <>
             {products.map((products: ProductType) => (
-              <div key={products.id} className={styles.produk__content__item}>
+              <Link
+                href={`/produk/${products.id}`}
+                key={products.id}
+                className={styles.produk__content__item}
+              >
                 <div className={styles.produk__content__item__image}>
                   <img src={products.image} alt={products.name} width={200} />
                 </div>
@@ -27,9 +32,9 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
                   {products.category}
                 </p>
                 <p className={styles.produk__content__item__price}>
-                  Rp {products.price.toLocaleString()}
+                  Rp {products.price.toLocaleString("id-ID")}
                 </p>
-              </div>
+              </Link>
             ))}
           </>
         ) : (

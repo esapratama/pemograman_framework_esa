a4c512d154a6e06de96f0921e5271fe06ab4a435
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-04-01 08:07:19 +0700

    Add Bagian 4 – Tambahkan SessionProvider

--- PRAKTIKUM/my-app14/src/pages/_app.tsx
+++ PRAKTIKUM/my-app14/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import AppShell from "@/components/layout/Appshell";
+import Navbar from "@/components/layout/navbar";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <AppShell>
+        <Component {...pageProps} />
+      </AppShell>
+    </SessionProvider>
+  );
 }

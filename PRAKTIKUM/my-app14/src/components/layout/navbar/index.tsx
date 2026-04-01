2a76903f7d6ebdbf2e8c8b44418a8070ca0de1d2
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-04-01 08:36:13 +0700

    Add Bagian 5 – Tambahkan Tombol Login & Logout

--- PRAKTIKUM/my-app14/src/components/layout/navbar/index.tsx
+++ PRAKTIKUM/my-app14/src/components/layout/navbar/index.tsx
@@ -1,11 +1,18 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import syles from './navbar.module.css';
 
 const Navbar = () => {
+  const { data } = useSession();
   return (
     <div className={syles.navbar}>
       <div className="big"> 
         Navbar
       </div>
+      {data ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
     </div>
   );
 };
--- PRAKTIKUM/my-app14/src/pages/auth/login.tsx
+++ PRAKTIKUM/my-app14/src/pages/auth/login.tsx
@@ -1,24 +1,37 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-// import styles from './login.module.css';
+import { signIn } from "next-auth/react"; // 1. Import signIn dari next-auth
 import styles from '../../views/auth/login/login.module.scss';
 
 const TampilanLogin = () => {
-    const {push} = useRouter();
+    const { push } = useRouter();
 
-    const handleLogin = () => {
-        // logic login disini
-        push('/produk');
+    const handleLogin = async () => {
+        const res = await signIn("credentials", {
+            redirect: false, 
+            email: "user@example.com", 
+            callbackUrl: "/produk",
+        });
+
+        if (res?.ok) {
+            push('/produk');
+        } else {
+            console.error("Login gagal:", res?.error);
+        }
     }
 
     return (
         <div className={styles.login}>
-            <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
-            <button onClick={() => handleLogin()}>Login</button> <br />
-            <h1 style={{color:"red", border:"1px solid red", borderRadius:"5px", padding: "5px"}}> belum punya akun</h1>
+            <h1 className="text-3xl font-bold text-blue-600">Halaman Login</h1>
+            
+            <button onClick={handleLogin}>Login</button> <br />
+            
+            <h1 style={{color:"red", border:"1px solid red", borderRadius:"5px", padding: "5px"}}>
+                Belum punya akun?
+            </h1>
             <Link href={"/auth/register"}>Ke Halaman Register</Link>
         </div>
     );
 }
 
-export default TampilanLogin
+export default TampilanLogin;

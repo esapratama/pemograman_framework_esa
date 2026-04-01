93d3fc3466f2dd721e5d02a7a65892beeed8ab77
Esa Pratama Putri <ptrprtm16@gmail.com>
2026-04-01 08:50:44 +0700

    Add  D. Menambahkan Data Tambahan (Full Name)

--- PRAKTIKUM/my-app14/src/pages/api/auth/[...nextauth].ts
+++ PRAKTIKUM/my-app14/src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   session: {
-    strategy: "jwt"
+    strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
@@ -12,37 +12,42 @@ export const authOptions: NextAuthOptions = {
       credentials: {
         fullname: { label: "Full Name", type: "text" },
         email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const user : any = {
+        const user: any = {
           id: "1",
           email: credentials?.email,
           password: credentials?.password,
-          fullname: credentials?.fullname
-        }
+          fullname: credentials?.fullname,
+        };
         if (user) {
-          return user
+          return user;
         } else {
-          return null
+          return null;
         }
-      }
-    })
+      },
+    }),
   ],
+
   callbacks: {
-    async jwt({ token, account, profile, user }: any) {
+    async jwt({ token, account, user }: any) {
       if (account?.provider === "credentials" && user) {
-        token.email = user.email
+        token.email = user.email;
+        token.fullname = user.fullname;
       }
-      return token
+      return token;
     },
     async session({ session, token }: any) {
       if (token.email) {
-        session.user.email = token.email
+        session.user.email = token.email;
       }
-      return session
+      if (token.fullname) {
+        session.user.fullname = token.fullname;
+      }
+      return session;
     },
   },
-};
+}; 
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);

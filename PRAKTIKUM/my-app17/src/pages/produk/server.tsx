d0dfa3f8cf748e3574268d965294542a28acbd97
esapratama <ptrprtm16@gmail.com>
2026-04-06 16:10:45 +0700

    Add my-app17

---
+++ PRAKTIKUM/my-app17/src/pages/produk/server.tsx
@@ -0,0 +1,25 @@
+import TampilanProduk from "../../views/produk";
+import { ProdukType } from "../../types/Produk.type";
+
+const halamanProdukServer = (props: { produk: ProdukType[] }) => {
+  const { produk } = props;
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk produk={produk} />
+    </div>
+  );
+};
+
+export default halamanProdukServer;
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const respone = await res.json();
+  // console.log("Data produk yang diambil dari API:", respone);
+  return {
+    props: {
+      produk: respone.data, 
+    },
+  };
+}

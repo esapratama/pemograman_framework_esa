fcc618a0da0baea1c785a69a7da4ecbf6b5156b2
esapratama <ptrprtm16@gmail.com>
2026-04-06 16:55:46 +0700

    Add G. Simpan Data Google ke Database

--- PRAKTIKUM/my-app17/src/pages/api/auth/[...nextauth].ts
+++ PRAKTIKUM/my-app17/src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import { signIn } from "@/utils/db/servicefirebase";
+import { signIn, signInWithGoogle } from "@/utils/db/servicefirebase";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
@@ -53,19 +53,26 @@ export const authOptions: NextAuthOptions = {
       token.role = user.role;
     }
     // Jika login dengan Google, tambahkan informasi yang diperlukan ke token
-if (account?.provider === "google") {
-  const data = {
-    fullname: user.name,
-    email: user.email,
-    image: user.image,
-    type: account.provider,
-  };
-  // console.log("Google login data", { data });
-  token.fullname = data.fullname;
-  token.email = data.email;
-  token.image = data.image;
-  token.type = data.type;
-}
+    // Jika login dengan Google, tambahkan informasi yang diperlukan ke token
+    if (account?.provider === "google") {
+      const data = {
+        fullname: user.name,
+        email: user.email,
+        image: user.image,
+        type: account.provider,
+      };
+
+      await signInWithGoogle(data, (result: any) => {
+        // Pastikan mengecek result.status sesuai dengan object yang dikirim
+        if (result.status) {
+          token.fullname = result.data.fullname;
+          token.email = result.data.email;
+          token.image = result.data.image;
+          token.type = result.data.type;
+          token.role = result.data.role;
+        }
+      });
+    }
     // console.log("jwt callback", { token, account, profile, user })
     return token;
   },

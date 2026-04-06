a7fab7f45e8ff9fa2f0f412ac6fbead667485fde
esapratama <ptrprtm16@gmail.com>
2026-04-06 13:45:43 +0700

    Add BAGIAN 2 – Handle Login di Frontend

--- PRAKTIKUM/my-app16/src/pages/auth/login.tsx
+++ PRAKTIKUM/my-app16/src/pages/auth/login.tsx
@@ -1,37 +1,11 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { signIn } from "next-auth/react"; // 1. Import signIn dari next-auth
-import styles from '../../views/auth/login/login.module.scss';
+import TampilanLogin from "../../views/auth/login";
 
-const TampilanLogin = () => {
-    const { push } = useRouter();
+const halamanLogin = () => {
+  return (
+    <>
+      <TampilanLogin />
+    </>
+  );
+};
 
-    const handleLogin = async () => {
-        const res = await signIn("credentials", {
-            redirect: false, 
-            email: "user@example.com", 
-            callbackUrl: "/produk",
-        });
-
-        if (res?.ok) {
-            push('/produk');
-        } else {
-            console.error("Login gagal:", res?.error);
-        }
-    }
-
-    return (
-        <div className={styles.login}>
-            <h1 className="text-3xl font-bold text-blue-600">Halaman Login</h1>
-            
-            <button onClick={handleLogin}>Login</button> <br />
-            
-            <h1 style={{color:"red", border:"1px solid red", borderRadius:"5px", padding: "5px"}}>
-                Belum punya akun?
-            </h1>
-            <Link href={"/auth/register"}>Ke Halaman Register</Link>
-        </div>
-    );
-}
-
-export default TampilanLogin;
+export default halamanLogin;
--- PRAKTIKUM/my-app16/src/utils/db/servicefirebase.ts
+++ PRAKTIKUM/my-app16/src/utils/db/servicefirebase.ts
@@ -9,7 +9,7 @@ import {
   where,
 } from "firebase/firestore";
 import app from "./firebase";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const db = getFirestore(app);
 
@@ -28,6 +28,21 @@ export async function retrieveDataByID(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(db, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+export async function signIn(email: string) {
+  const q = query(collection(db, "users"), where("email", "==", email));
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (data.length > 0) {
+    return data[0];
+  } else {
+    return null;
+  }
 }
 
 // Fungsi pendaftaran User (Sign Up)
